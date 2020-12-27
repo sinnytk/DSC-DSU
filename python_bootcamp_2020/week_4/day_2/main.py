429b3b2d2deb1896aa4c3c0f00764b1ed80daafd
Muhammad Taha Khan <mtahakhan93@gmail.com>
2020-12-27 21:41:08 +0500

    Added Day_2 lecture notes and files

--- python_bootcamp_2020/week_4/day_2/main.py
+++ python_bootcamp_2020/week_4/day_2/main.py
@@ -1,109 +1,130 @@
-from flask import Flask, render_template,redirect,url_for,session,request
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from wtforms import Form, StringField, PasswordField, validators
 import pymongo as pym
+import datetime
 
 app = Flask(__name__)
-app.secret_key = "secret123"
+app.secret_key='secret123'
 
 connection = pym.MongoClient("mongodb+srv://mtk:1234@cluster0.pepx2.mongodb.net/test?retryWrites=true&w=majority")
-db = connection.get_database("test")
+db = connection.get_database('test')
 
-User_details = pym.collection.Collection(db,"User_details")
-tweets = pym.collection.Collection(db,"tweets")
+user_detail = pym.collection.Collection(db, 'User_details')
+tweets = pym.collection.Collection(db, 'tweets')
 
+@app.route('/index',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+            tweet = request.form['tweet']            
+            ct = datetime.datetime.now()
+            
+            user_id = tweets.insert_one(
+            {   "email" : session["username"],
+                "tweet" : tweet,
+                "name" : session['name'], 
+                "time" : str(ct.date())
+            })
+    
+            #flash('Tweet added', 'success')
+            
+            answer = get_tweets(session['username'])
+            return render_template('Home.html',dictionary=answer)
+
+    answer = get_tweets(session['username'])                   
+    return render_template('Home.html',dictionary=answer)
+
+# Register Form Class
 class RegisterForm(Form):
-    email = StringField("Email",[validators.Email()])
-    name = StringField("Username",[validators.Length(min=1,max=50)])
-    password = PasswordField("Password",[validators.DataRequired()
-        ,validators.EqualTo("confirm",message="Password Doesnt match")])
-    confirm = PasswordField("Confirm Password")
+    email = StringField('Email', [validators.Email()])
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
-@app.route("/",methods=['GET','POST'])
+
+# User Register
+@app.route('/', methods=['GET', 'POST'])
 def register():
     
-    form = RegisterForm(request.form)
+    users = user_detail
     
-    if request.method == "POST":
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
         name = form.name.data
         email = form.email.data
         password = form.password.data
-        
-        user = User_details.insert_one(
-                {"name":name,
-                 "email":email,
-                 "password":password
-                }
-                )
-        if(user):
-            print("Inserted!")
-        
-        return render_template("Login_template.html")
-    
-    return render_template("Register_template.html",form=form)
 
-
-@app.route("/index",methods=['GET','POST'])
-def index():
-    
-    if request.method == 'POST':
-        Tweet = request.form['tweet']
+        myid = users.insert_one(
+            {   "name" : name,
+                "email" : email,
+                "password" : password
+            })
         
-        user_tweet = tweets.insert_one(
-                {
-                    "tweet" : Tweet,
-                    "name" : session['name'],
-                    "date" : "26-12-2020",
-                    "email":session['email']
-                        })
-            
-        #Tweets = get_tweets(session['email'])
-    
-    Tweets = get_tweets(session['email'])
-    
-    return render_template("Social_media_template.html",dictionary=Tweets)
+        flash('You are now registered and can log in', 'success')
 
-def get_tweets(email):
-    user_tweets = tweets.find({"email":email})
-    user_tweet = {}
-    y = {}
-    for documents in user_tweets:
-        y = documents
-        key = y['_id']
-        user_tweet.setdefault(key,[])
-        user_tweet[key].append(y['name'])
-        user_tweet[key].append(y['tweet'])
-        user_tweet[key].append(y['date'])
-    
-    return user_tweet
-        
+        redirect(url_for('login'))
+        #return render_template('Login.html') 
+    return render_template('Register.html', form=form)
 
-@app.route("/login",methods=['GET','POST'])
+# User login
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        email = request.form['email']
-        password = request.form['password']
+    user = user_detail
+    if request.method == 'POST':
+        # Get Form Fields
+        email = request.form['username']
+        password_candidate = request.form['password']
         
-        check_user = User_details.find_one({"email":email,"password":password})
-        if check_user:
-            session['logged_in'] = True
-            session['email'] = email
-            session['name'] = check_user['name']
-            
-            return redirect(url_for("index"))
-        else:
-            return render_template("Login_template.html")
+        response = user.find_one({'email': email})
         
-        
-    return render_template("Login_template.html")
 
-@app.route("/logout")
+        if response:
+            password = response['password']
+
+            # Compare Passwords
+            if password_candidate == password:
+                # Passed
+                session['logged_in'] = True
+                session['username'] = email
+                session['name'] = response['name']
+                
+                flash('You are now logged in', 'success')
+                return redirect(url_for('index'))
+            else:
+                error = 'Invalid login'
+                return render_template('Login.html', error=error)
+        else:
+            error = 'Email not found'
+            return render_template('Login.html', error=error)
+
+    return render_template('Login.html')
+
+# Logout
+@app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for("login"))
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 
+def get_tweets(email):
+    pid = tweets.find({"email":session['username']})
+                
+    user_tweets = {}
+    y = {}
+    for docs in pid:
+        y = docs
+        key = y["_id"]
+        user_tweets.setdefault(key,[])
+        user_tweets[key].append(y["name"])
+        user_tweets[key].append(y["tweet"])
+        user_tweets[key].append(y["time"])
+    
+    return user_tweets
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
+    
